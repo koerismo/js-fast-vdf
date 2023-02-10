35729557963361acce2fa276a831f1faa1e18c45
Koerismo <jadon@Jadon-Laptop.local>
2023-02-10 13:16:00 -0500

    Rename exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import * as parse from './parse.js';
 import * as core from './core.js';
+import { KeyV, KeyVSet, KeyVRoot } from './types.js';
 
 export default parse;
 
 export {
-	parse,
+	parse as vdf,
 	core,
+
+	KeyV,
+	KeyVSet,
+	KeyVRoot,
 }
