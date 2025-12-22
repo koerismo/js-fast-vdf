5fe70fc8683cc91383f7decc57ea4e154dd5e516
Koerismo <56563658+koerismo@users.noreply.github.com>
2025-12-22 13:57:14 -0500

    feat: Update speedtest, normalize tsconfig

--- test/speed/speed.mjs
+++ test/speed/speed.mjs
@@ -1,6 +1,5 @@
 import { readFileSync } from 'node:fs';
 import * as vdf from '../../dist/index.js';
-import { DumpQuotationType } from '../../dist/types.js';
 
 const TRIALS = 20;
 const vmfString = readFileSync('test/speed/pl_goldrush_halloween.vmf', 'utf-8');
@@ -16,7 +15,7 @@ function trial(parseOpts) {
 	const end_parse = performance.now();
 
 	for ( let i=0; i<TRIALS; i++ ) {
-		dummy_out = dummy_in.dump({ quote: DumpQuotationType.Auto, indent: '\t', escapes: parseOpts.escapes });
+		dummy_out = dummy_in.dump({ quote: vdf.DumpQuotationType.Auto, indent: '\t', escapes: parseOpts.escapes });
 	}
 
 	const end_dump = performance.now();
